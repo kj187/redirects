48db9ca4407abeac40ce23a39f8c73c636c9ff51
Michael Klapper <development@morphodo.com>
2012-03-19 13:59:27 +0100

    Fix spelling issue

--- Tests/Unit/Domain/Model/RedirectTest.php
+++ Tests/Unit/Domain/Model/RedirectTest.php
@@ -266,7 +266,7 @@ class Tx_Redirects_Domain_Model_RedirectTest extends Tx_Extbase_Tests_Unit_BaseT
 	 */
 	public function getCountryCodeReturnsInitialValueForInteger() {
 		$this->assertSame(
-			0,
+			'',
 			$this->fixture->getCountryCode()
 		);
 	}
@@ -275,10 +275,10 @@ class Tx_Redirects_Domain_Model_RedirectTest extends Tx_Extbase_Tests_Unit_BaseT
 	 * @test
 	 */
 	public function setCountryCodeForIntegerSetsCountryCode() {
-		$this->fixture->setCountryCode(12);
+		$this->fixture->setCountryCode('DE');
 
 		$this->assertSame(
-			12,
+			'DE',
 			$this->fixture->getCountryCode()
 		);
 	}
